8d962b56134cbb5e4bc9e5bbe474a9ca09bc921d
Ans10hu <Anshukumarsharma392@gmail.com>
2025-04-28 17:45:38 +0530

    class created

---
+++ src/Tasks/ButtonStyle.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+ class ButtonStyle extends Component {
+    constructor(){
+        super();
+        this.state ={
+            isBlue : true,
+        };
+    }
+  render() {
+    return (
+      <div>ButtonStyle</div>
+    )
+  }
+}
+
+export default ButtonStyle
